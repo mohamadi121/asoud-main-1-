7311ce12618e90ae225024ab1bbfa613ffeefa5c
Mark Judas <fardinramezani97@gmail.com>
2025-06-16 13:40:07 +0330

    added the delete endpoint for user price_inquiry

--- asoud-main/apps/price_inquiry/urls/user.py
+++ asoud-main/apps/price_inquiry/urls/user.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from apps.price_inquiry.views.user import (
     InquiryCreateView,
+    InquiryDeleteView,
     InquiryImageUploadView,
     InquirySendSetView,
     InquiryListView,
@@ -14,6 +15,7 @@ app_name = 'user_inquiry'
 urlpatterns = [
     path('', InquiryListView.as_view(), name='list-inquiry'),
     path('create/', InquiryCreateView.as_view(), name='create-inquiry'),
+    path('<str:pk>/delete', InquiryDeleteView.as_view(), name='delete-inquiry'),
     path('<str:pk>/', InquiryDetailView.as_view(), name='inquiry-detail'),
     path('<str:pk>/image/', InquiryImageUploadView.as_view(), name='add-inquiry-image'),
     path('<str:pk>/send/', InquirySendSetView.as_view(), name='inquiry-send'),
