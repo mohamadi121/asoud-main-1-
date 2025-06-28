8f0d022b47cf5c8efaaad93c28e2012186e72a0f
Mark Judas <fardinramezani97@gmail.com>
2025-06-28 22:51:35 +0330

    add advertisement and advimg admins

--- asoud-main/apps/advertise/admin.py
+++ asoud-main/apps/advertise/admin.py
@@ -1,5 +1,6 @@
 from apps.base.admin import admin, BaseAdmin
 
+from apps.advertise.models import Advertisement, AdvImage
 # Register your models here.
 
 class AdvertisementAdmin(admin.ModelAdmin):
@@ -19,3 +20,13 @@ class AdvertisementAdmin(admin.ModelAdmin):
         'email',
         'keywords',
     ) + BaseAdmin.fields
+
+
+class AdvertiseImageAdmin(BaseAdmin):
+    list_display = ('advertise', 'image')
+    fields = ('advertise', 'image')
+    readonly_fields = ('advertise', )
+
+
+admin.site.register(Advertisement, AdvertisementAdmin)
+admin.site.register(AdvImage, AdvertiseImageAdmin)
